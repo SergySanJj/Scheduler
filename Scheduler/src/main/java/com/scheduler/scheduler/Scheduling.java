cc60d5c7f36759768bd7540d937cb97fe892f5c8
SergySanJj <isara.isara8@gmail.com>
2019-11-25 22:04:51 +0200

    Add command line argument for config

--- Scheduler/src/main/java/com/scheduler/scheduler/Scheduling.java
+++ Scheduler/src/main/java/com/scheduler/scheduler/Scheduling.java
@@ -8,13 +8,17 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Scheduling {
     private static final String schema = "res/processes.xsd";
+    private static final String defaultConfig = "res/defaultConfig.xml";
+    private static String config;
+    private static Simulation simulation;
 
     public static void main(String[] args) {
-        StAXParser<Simulation> parser = new StAXParser<>(new SimulationHandler(), schema);
-        Simulation simulation = parser.parse("res/config.xml");
+        tryLoadConfig(args);
 
         FairShare fairShare = new FairShare();
         File resFile = new File("res/Summary-Results.txt");
@@ -34,5 +38,35 @@ public class Scheduling {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void tryLoadConfig(String[] args) {
+        if (args.length != 1) {
+            Logger.getLogger("main").log(Level.INFO, "Args are empty or have more than 1 arg");
+            loadDefaoultConfig();
+        } else {
+            File f = new File(args[0]);
+            if (!(f.exists())) {
+                Logger.getLogger("main").log(Level.INFO, "Scheduling: error, file '" + f.getName() + "' does not exist.");
+                loadDefaoultConfig();
+            }
+            if (!(f.canRead())) {
+                Logger.getLogger("main").log(Level.INFO, "Scheduling: error, read of " + f.getName() + " failed.");
+                loadDefaoultConfig();
+            }
+            config = args[0];
+        }
+
+        StAXParser<Simulation> parser = new StAXParser<>(new SimulationHandler(), schema);
+        simulation = parser.parse(config);
+        if (simulation == null){
+            Logger.getLogger("main").log(Level.INFO, "File " + config + " can't be parsed" );
+            loadDefaoultConfig();
+            simulation = parser.parse(defaultConfig);}
+    }
+
+    private static void loadDefaoultConfig() {
+        Logger.getLogger("main").log(Level.INFO, "Loading default config");
+        config = defaultConfig;
     }
 }
